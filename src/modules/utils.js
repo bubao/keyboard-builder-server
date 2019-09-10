6635dc1daac3acfce21762be984c4c9f2c5654f2
bubao <asd565586630@gmail.com>
2019-09-10 18:31:11 +0800

    log is clean

--- src/modules/utils.js
+++ src/modules/utils.js
@@ -3,7 +3,7 @@
  * @Author: bubao
  * @Date: 2019-09-09 16:02:22
  * @LastEditors: bubao
- * @LastEditTime: 2019-09-10 18:21:56
+ * @LastEditTime: 2019-09-10 18:30:59
  */
 
 const crypto = require("crypto");
@@ -20,6 +20,7 @@ function key() {
 }
 
 function clean(where) {
+	console.log("is clean", where);
 	if (where !== undefined && where !== "/") {
 		return exec(`rm -rf ${where}`);
 	}
--- src/premake.js
+++ src/premake.js
@@ -3,7 +3,7 @@
  * @Author: bubao
  * @Date: 2019-09-09 23:16:30
  * @LastEditors: bubao
- * @LastEditTime: 2019-09-10 18:11:00
+ * @LastEditTime: 2019-09-10 18:26:59
  */
 const { writeFile, exec } = require("./modules/promisify");
 const { CORE } = require("./modules/const");
@@ -19,7 +19,7 @@ async function premake(res, files, randomPatch, template, make) {
 	// Copy all the files.
 	for (const file in files) {
 		const fileName = file.replace("tmk_firmware", randomPatch);
-		writeFile(fileName, files[file]).catch(reason => {
+		await writeFile(fileName, files[file]).catch(reason => {
 			console.log(reason);
 			utils.sendError(res, "Failed to initialize.", randomPatch, 400);
 		});
