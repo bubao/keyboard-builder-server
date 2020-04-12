a8dfaca8f8c1e2682357e3b7f5fa83ffac1789bc
genokolar <genokolar@gmail.com>
2020-04-12 14:13:37 +0800

    xiugai

--- src/modules/const.js
+++ src/modules/const.js
@@ -6,7 +6,7 @@
  * @LastEditTime: 2019-09-09 23:35:35
  */
 const PORT = 5004;
-const TMP = "/var/tmp/keyboard";
+const TMP = "/home/geno/SOURCE/tmp/skeyboard";
 const CORE = require("../../config.json");
 
 module.exports = {
